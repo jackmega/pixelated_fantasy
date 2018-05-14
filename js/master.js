a84fcf223dfb793e66a5c0a8dac245abdce5dc6d
jackmega <jackmega@live.com>
2018-05-14 10:37:39 -0600

    mobile friendly

---
+++ js/master.js
@@ -0,0 +1,10 @@
+function navOpen() {
+	document.getElementById('dropdown').style.display = "block";
+	document.getElementById('mobile-btn').style.display = 'none';
+	document.getElementById('mobile-close').style.display = 'block';
+}
+function navClose() {
+	document.getElementById('dropdown').style.display = "none";
+	document.getElementById('mobile-btn').style.display = 'block';
+	document.getElementById('mobile-close').style.display = 'none';
+}
